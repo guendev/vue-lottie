46beca28524cd189fd03f1d794b3ecff5383e58d
coder <dnstylish@gmail.com>
2022-06-03 08:10:56 +0700

    update packpage

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-export * from './plugins/lottie'
-export { default as VueLottiePlayer } from './components/VueLottiePlayer.vue'
+import type { App } from 'vue'
+import VueLottiePlayer from "./components/VueLottiePlayer.vue"
+
+const plugin = {
+    install(app: App) {
+        app.component('VueLottiePlayer', VueLottiePlayer)
+    }
+}
+
+export default plugin
+
+export {
+    VueLottiePlayer
+}
